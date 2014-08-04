d05164371b56beaeaab463e2031adc2e3bf0d3bf
Steven Noonan <steven@uplinklabs.net>
2014-08-04 10:00:32 -0700

    common.mk: check whether -std=gnu11 and -fno-strict-aliasing are supported
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- common.mk
+++ common.mk
@@ -21,6 +21,30 @@ ifneq ($(findstring CYGWIN,$(OSNAME)),)
 OSNAME     := Cygwin
 endif
 
+# cc-option: Check if compiler supports first option, else fall back to second.
+#
+# This is complicated by the fact that unrecognised -Wno-* options:
+#   (a) are ignored unless the compilation emits a warning; and
+#   (b) even then produce a warning rather than an error
+# To handle this we do a test compile, passing the option-under-test, on a code
+# fragment that will always produce a warning (integer assigned to pointer).
+# We then grep for the option-under-test in the compiler's output, the presence
+# of which would indicate an "unrecognized command-line option" warning/error.
+#
+# Usage: cflags-y += $(call cc-option,$(CC),-march=winchip-c6,-march=i586)
+cc-option = $(shell if test -z "`echo 'void*p=1;' | \
+              $(1) $(2) -S -o /dev/null -xc - 2>&1 | grep -- $(2) -`"; \
+              then echo "$(2)"; else echo "$(3)"; fi ;)
+
+# cc-option-add: Add an option to compilation flags, but only if supported.
+# Usage: $(call cc-option-add,CFLAGS,CC,-march=winchip-c6)
+cc-option-add = $(eval $(call cc-option-add-closure,$(1),$(2),$(3)))
+define cc-option-add-closure
+    ifneq ($$(call cc-option,$$($(2)),$(3),n),n)
+        $(1) += $(3)
+    endif
+endef
+
 ifneq ($(shell type -P clang),)
 CC         := clang
 else
@@ -32,7 +56,10 @@ AR         := ar rcu
 RM         := rm -f
 CPPFLAGS   := -Wall -Werror
 CFOPTIMIZE := -O2
-CFLAGS     := $(CFOPTIMIZE) -std=gnu11 -fno-strict-aliasing $(CPPFLAGS)
+CFLAGS     := $(CFOPTIMIZE)
+$(call cc-option-add,CFLAGS,CC,-std=gnu11)
+$(call cc-option-add,CFLAGS,CC,-fno-strict-aliasing)
+CFLAGS     += $(CPPFLAGS)
 LDFLAGS    :=
 
 ifeq ($(OSNAME),Darwin)
